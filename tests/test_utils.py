d961960709707bf3481ec4e50952d9698968442f
arsen <kuzhamuratov@phystech.edu>
2022-05-07 14:23:34 +0300

    add test utils module

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,39 +1,76 @@
 import pandas as pd
 import numpy as np
 
-import os
-import sys
-import inspect
 
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir)
+from ml_project import utils
 
-import ml_project
+FILENAME = './file.sav'
 
-# PATH_TO_REAL_DATASET = '../ml_project/data/heart_cleveland_upload.csv'
-# REAL_COLUMNS = pd.read_csv(PATH_TO_REAL_DATASET).columns
-# FAKE_DATA_SMALL = pd.DataFrame.from_dict(
-#     {column: [0] for column in REAL_COLUMNS}
-#     )
+REAL_COLUMNS = ['age', 'sex','cp', 'trestbps', 
+                'chol', 'fbs', 'restecg', 'thalach',
+                'exang', 'oldpeak', 'slope', 'ca', 'thal', 'condition']
+FAKE_DATA_SMALL = pd.DataFrame.from_dict(
+    {'a1': [1] * 10 + [2] * 10, 
+    'a2': [1] * 20, 
+    'a3': list(range(20)),
+    'a4': list(range(9)) + [0] * 11}
+    )
 
-# FAKE_DATA_BIG = pd.DataFrame.from_dict({
-#     column: np.random.randint(low=0, high=1 + np.random.randint(20), size=1000) 
-#     for column in REAL_COLUMNS
-#     })
+FAKE_DATA_MID = pd.DataFrame.from_dict({
+    column: list(range(ind)) + [0] * (len(REAL_COLUMNS) - ind)
+    for ind, column in enumerate(REAL_COLUMNS, 1)
+    })
+FAKE_DATA_MID['condition'] = np.random.randint(2, size=len(REAL_COLUMNS))
 
-
-def test_logistic_regression_model():
-    pass
-
-def test_one_hot_encoding():
-    pass
+SINGLE_LINE = [0] * (len(REAL_COLUMNS) - 1)
+VALID_SINGLE_LINE = pd.DataFrame.from_dict({
+    column: [value]
+    for column, value in zip(REAL_COLUMNS[:-1], SINGLE_LINE)
+    })
 
 def test_divide_columns_by_type():
-    pass
+    result = utils.divide_columns_by_type(FAKE_DATA_SMALL, threshold=10)
+    assert ['a1', 'a2', 'a4'] == result['categorical']
+    assert ['a3'] == result['continuous']
 
-def test_feature_extraction():
-    pass
+def test_divide_columns_by_type_mid_dataset():
+    result = utils.divide_columns_by_type(FAKE_DATA_MID, threshold=10)
+    assert 11 == len(result['categorical'])
+    assert 3 == len(result['continuous'])
 
-def test_feature_extraction_big():
-    pass
+def test_train_feature_extraction_grad_boosting():
+    features = utils.feature_extraction(
+        data=FAKE_DATA_MID,
+        model_type='GradientBoosting', 
+        filename=FILENAME,
+        train=True)
+    assert 14 == len(features['labels'])
+    assert np.where(FAKE_DATA_MID.drop(columns=['condition']) == features['features'])
+
+def test_train_feature_extraction_log_reg():
+    features = utils.feature_extraction(
+        data=FAKE_DATA_MID,
+        model_type='LogisticRegression', 
+        filename=FILENAME,
+        train=True)
+    assert 14 == len(features['labels'])
+    assert 55 + 3 == len(features['features'].columns)
+
+def test_valid_feature_extraction_log_reg():
+    features = utils.feature_extraction(
+        data=FAKE_DATA_MID,
+        model_type='LogisticRegression', 
+        filename=FILENAME,
+        train=False)
+    assert 14 == len(features['labels'])
+    assert 55 + 3 == len(features['features'].columns)
+
+def test_valid_feature_extraction_log_reg():
+    features = utils.feature_extraction(
+        data=VALID_SINGLE_LINE,
+        model_type='LogisticRegression', 
+        filename=FILENAME,
+        train=False)
+    assert None == features['labels']
+    assert 55 + 3 == len(features['features'].columns)
+    assert 5 == np.unique(features['features'].values).shape[0]
