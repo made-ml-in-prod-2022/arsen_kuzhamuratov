e7b498bc1b8af7363df4f8ad4cca17f76370ff83
arsen <kuzhamuratov@phystech.edu>
2022-05-07 18:26:57 +0300

    change test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,16 +1,20 @@
 import argparse
 import os
+import yaml
 from ml_project.main import main
 
+
+
+
 TRAINING_NAMESPACE_GRAD_BOOST = argparse.Namespace(
-    config_path='../configs/gradient_boosting.yaml',
-    data_path='../ml_project/data/heart_cleveland_upload.csv',
+    config_path='./configs/gradient_boosting.yaml',
+    data_path='./ml_project/data/heart_cleveland_upload.csv',
     train=True,
     debug=True,
     inference=False,
-    stats_path='../outputs/column_mean_std_stats.sav',
-    results_path='../outputs/prediction.csv',
-    log_path='../outputs/file.log',
+    stats_path='./outputs/column_mean_std_stats.sav',
+    results_path='./outputs/prediction.csv',
+    log_path='./outputs/file.log',
     )
 
 def test_train_grad_boosting(capsys):
@@ -24,14 +28,14 @@ def test_train_grad_boosting(capsys):
 
 
 INFERENCE_NAMESPACE_GRAD_BOOST = argparse.Namespace(
-    config_path='../configs/gradient_boosting.yaml',
-    data_path='../ml_project/data/heart_cleveland_upload.csv',
+    config_path='./configs/gradient_boosting.yaml',
+    data_path='./ml_project/data/heart_cleveland_upload.csv',
     train=False,
     debug=True,
     inference=True,
-    stats_path='../outputs/column_mean_std_stats.sav',
-    results_path='../outputs/prediction.csv',
-    log_path='../outputs/file.log',
+    stats_path='./outputs/column_mean_std_stats.sav',
+    results_path='./outputs/prediction.csv',
+    log_path='./outputs/file.log',
     )
 
 
@@ -39,7 +43,7 @@ def test_inference_grad_boosting(capsys):
     res = main(INFERENCE_NAMESPACE_GRAD_BOOST)
     captured = capsys.readouterr()
     assert '' == captured.err
-    assert os.path.exists('../outputs/prediction.csv')
+    assert os.path.exists('./outputs/prediction.csv')
 
 
 TRAINING_NAMESPACE_LOG_REG = argparse.Namespace(
